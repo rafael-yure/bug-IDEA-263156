9c6986efef318e3a9cde8460cb12f12d9089b3fa
Rafael Braga <rafael.braga@picpay.com>
2021-03-15 21:05:29 -0300

    Fixing imports and enabling @FeignClients

--- src/main/java/com/example/demo/client/CustomerClient.java
+++ src/main/java/com/example/demo/client/CustomerClient.java
@@ -2,9 +2,10 @@ package com.example.demo.client;
 
 import com.example.demo.dto.CustomerDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient
+@FeignClient(name = "customer")
 public interface CustomerClient {
 
     @GetMapping
